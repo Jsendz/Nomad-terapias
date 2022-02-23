768c99256e4038a76fb26119f039a2356378247a
Jsendz <joshuasendzul@gmail.com>
2022-02-23 16:08:54 +0100

    first commi1

---
+++ src/components/footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <>
+      <div>
+          <h1>Request</h1>
+            <hr />
+            <input type="email" /><button>Request</button>
+
+      </div>
+      <div>
+        <h1>Let's Connect!</h1>
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+            <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+
+      </div>
+      <div>
+        <h1>Contact</h1>
+
+          <i class="fa fa-phone" aria-hidden="true">+376 612400</i>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default footer
